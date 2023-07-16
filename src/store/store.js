2450b21ef2c447ddfe8251f162c638367f7fb88a
Colson <colsoncalifornication@gmail.com>
2023-07-16 11:02:07 +0545

    manage listener functions

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,18 @@
 const createStore = (reducer) => {
   let state;
+  let listeners = [];
 
   const getState = () => state;
+
   const dispatch = () => {};
-  const subscribe = () => {};
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
 
   return { getState, dispatch, subscribe };
 };
