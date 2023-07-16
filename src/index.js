c1b2a4dcc79b68d546a3637d91a8bb566ef27aa3
Colson <colsoncalifornication@gmail.com>
2023-07-16 11:21:29 +0545

    refractor and implement store state and dispatch an action

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import { store } from './store/store';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<App />);
+root.render(<App state={store.getState()} dispatch={store.dispatch} />);
+
+const render = () => {
+  document.getElementById('count').text = store.getState();
+};
+render();
+
+store.subscribe(render);
+
+document.getElementById('plusButton').addEventListener('click', () => {
+  store.dispatch({
+    type: 'increment',
+  });
+});
--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,14 @@
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
+import { createStore } from './createStore';
 
-  const getState = () => state;
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach((listener) => listener());
-  };
-
-  const subscribe = (listener) => {
-    listeners.push(listener);
-
-    return () => {
-      listeners = listeners.filter((l) => l !== listener);
-    };
-  };
-
-  dispatch({});
-  return { getState, dispatch, subscribe };
+const countReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    default:
+      return state;
+  }
 };
+
+export const store = createStore(countReducer);
